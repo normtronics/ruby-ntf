45c305ff2e0d760d2f602e9ce89987928154dc09
Christopher Norman <chris@artofdigging.com>
2024-03-19 15:34:28 -0700

    add correct meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,45 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Providers } from './providers'
 
+const title = 'The Rose Crib NFTs'
+const description = 'The Rose Crib NFTs'
+
 export const metadata: Metadata = {
-  title: "Claim NFTs via qr codes | thirdweb Engine",
+  title,
+  description,
+  applicationName: "The Rose Crib NFTs",
+  authors: [{
+    name: 'The Rose Crib',
+    url: 'https://therosecrib.xyz/'
+  }],
+  keywords: 'music, nft, nfts',
+  openGraph: {
+    type: 'website',
+    title,
+    description,
+    emails: ['info@therosecrib.com'],
+    siteName: 'The Rose Crib NFTs',
+    url: 'https://therosecrib.xyz/',
+    images: [{
+      url: 'https://therosecrib.xyz/bb10NFT.png',
+      secureUrl: 'https://therosecrib.xyz/bb10NFT.png',
+      type: 'png',
+    }],
+    countryName: 'USA',
+  },
+  twitter: {
+    card: 'summary',
+    site: 'https://therosecrib.xyz/',
+    creator: 'Ruby Mountain',
+    description,
+    title,
+    images: [{
+      url: 'https://therosecrib.xyz/bb10NFT.png',
+      secureUrl: 'https://therosecrib.xyz/bb10NFT.png',
+      type: 'webp',
+    }],
+  },
+  creator: 'The Rose Crib',
 };
 
 export default function RootLayout({
