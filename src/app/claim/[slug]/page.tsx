9faab50dfa5ef1106ae58f11f726599840f01c84
Christopher Norman <normtronics@gmail.com>
2024-04-18 15:58:34 -0700

    Fix slugs

--- src/app/claim/[slug]/page.tsx
+++ src/app/claim/[slug]/page.tsx
@@ -1,85 +1,18 @@
 
-import Button from "../../components/Button";
-import { MediaRenderer } from "../../components/MediaRenderer";
-import prisma from "../../utils/prisma";
-import styles from "../../styles/Claim.module.css";
+import Button from "../../../components/Button";
+import { MediaRenderer } from "../../../components/MediaRenderer";
+import styles from "../../../styles/Claim.module.css";
 import { Header } from "@/components/Header/Header";
 import { Metadata, ResolvingMetadata } from "next";
 import React from "react";
-import { CountdownTimer } from "@/components/count-down/count-down";
 import { SimpleFooter } from "@/components/footer/footer";
-import request, { gql, GraphQLClient } from "graphql-request";
+import { gql, GraphQLClient } from "graphql-request";
 
 const title = 'Ruby Mountain NFTs'
 const description = 'Ruby Mountain NFTs'
 
-const metadata: Metadata = {
-  title,
-  description,
-  applicationName: "Ruby Mountain NFTs",
-  authors: [{
-    name: 'Ruby Mountain',
-    url: 'https://nft.rubymountain.xyz/'
-  }],
-  keywords: 'music, nft, nfts',
-  openGraph: {
-    type: 'website',
-    title,
-    description,
-    emails: ['info@therosecrib.com'],
-    siteName: 'Ruby Mountain NFTs',
-    url: '',
-    images: [{
-      url: 'https://nft.rubymountain.xyz/bb10NFT.png',
-      secureUrl: 'https://nft.rubymountain.xyz/bb10NFT.png',
-      type: 'png',
-    }],
-    countryName: 'USA',
-  },
-  twitter: {
-    card: 'summary',
-    site: 'https://nft.rubymountain.xyz/',
-    creator: 'Ruby Mountain',
-    description,
-    title,
-    images: [{
-      url: 'https://nft.rubymountain.xyz/bb10NFT.png',
-      secureUrl: 'https://nft.rubymountain.xyz/bb10NFT.png',
-      type: 'png',
-    }],
-  },
-  creator: 'Ruby Mountain',
-}
-
 type Props = {
-  params: { id: string }
-  searchParams: { [key: string]: string | string[] | undefined }
-}
-
-export async function generateMetadata(
-  { params, searchParams }: Props,
-  parent: ResolvingMetadata
-): Promise<Metadata> {
-  // read route params
-  const id = searchParams.id
- 
-  return {
-    ...metadata,
-    openGraph: {
-      type: 'website',
-      title,
-      description,
-      emails: ['info@therosecrib.com'],
-      siteName: 'The Rose Crib NFTs',
-      url: `https://nft.rubymountain.xyz/claim?id=${id}`,
-      images: [{
-        url: 'https://nft.rubymountain.xyz/bb10NFT.png',
-        secureUrl: 'https://nft.rubymountain.xyz/bb10NFT.png',
-        type: 'png',
-      }],
-      countryName: 'USA',
-      },
-  }
+  params: { slug: string } 
 }
 
 interface NFT {
@@ -89,11 +22,57 @@ interface NFT {
   slug: string,
   title: string,
   creator: string,
-  atributes: { [key: string]: string }
+  atributes: { [key: string]: string },
+  end: string,
+  start: string,
 }
 
-async function getData(id: string) {
-  console.log(process.env.NEXT_PUBLIC_HYGRAPH_URL, process.env.NEXT_PUBLIC_HYGRAPH_TOKEN)
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const { slug } = params
+  const { nft } = await getData(slug)  
+  const image = nft.image.replace("ipfs://", "https://ipfs.io/ipfs/")
+  return {
+    title,
+    description,
+    applicationName: "Ruby Mountain NFTs",
+    authors: [{
+      name: 'Ruby Mountain',
+      url: 'https://nft.rubymountain.xyz/'
+    }],
+    keywords: 'music, nft, nfts',
+    twitter: {
+      card: 'summary',
+      site: 'https://nft.rubymountain.xyz/',
+      creator: 'Ruby Mountain',
+      description,
+      title,
+      images: [{
+        url: image,
+        secureUrl: image,
+        type: 'png',
+      }],
+    },
+    creator: 'Ruby Mountain',
+    openGraph: {
+      type: 'website',
+      title,
+      description,
+      emails: ['info@therosecrib.com'],
+      siteName: 'The Rose Crib NFTs',
+      url: `https://nft.rubymountain.xyz/${slug}`,
+      images: [{
+        url: image,
+        secureUrl: image,
+      }],
+      countryName: 'USA',
+    },
+  }
+}
+
+async function getData(slug: string) {
   const graphQLClient = new GraphQLClient(process.env.NEXT_PUBLIC_HYGRAPH_URL || '', {
     headers: {
       authorization: `Bearer ${process.env.NEXT_PUBLIC_HYGRAPH_TOKEN}`,
@@ -101,8 +80,8 @@ async function getData(id: string) {
   })
 
   const query = gql`
-    query getNft($id: String!) {
-      nft(where: {slug: $id}) {
+    query getNft($slug: String!) {
+      nft(where: {slug: $slug}) {
         id
         description
         image
@@ -110,12 +89,15 @@ async function getData(id: string) {
         title
         creator
         atributes
+        nftype
+        start
+        end
       }
     }
   `
 
   const variables = {
-    id: "ruby-mountain-rebirth-two",
+    slug
   }
   
   const data = await graphQLClient.request<{ nft: NFT }>(query, variables)
@@ -123,17 +105,21 @@ async function getData(id: string) {
   return { nft: data.nft};
 }
 
-export default async function ClaimPage({
-  searchParams,
-}: {
-  searchParams: { id: string };
-}) {
-  const { nft } = await getData(searchParams.id)
+const checkExpired = (date: string) => {
+  const today = new Date() 
+  const end = new Date(date)
+  return today > end
+}
 
-  const THREE_DAYS_IN_MS = 3 * 24 * 60 * 60 * 1000;
-  const NOW_IN_MS = 1711727137973
+export default async function ClaimPage({ params }: { params: { slug: string } }) {
+  const { slug } = params
+  const { nft } = await getData(slug)
+  console.log(nft)
+  const endDate = new Date(nft.end).toLocaleString()
 
-  const dateTimeAfterThreeDays = NOW_IN_MS + THREE_DAYS_IN_MS;
+  if(checkExpired(endDate)) {
+    return null
+  }
  
   return (
     <>
@@ -152,9 +138,9 @@ export default async function ClaimPage({
           <div className="claimSection">
             <div className="countdown">
               Give away closes on
-              <div className="date">April, 28th at 11:59 PM</div>
+              <div className="date">{endDate}</div>
             </div>
-            <Button id={searchParams.id} />
+            <Button id={slug} />
             <p className="disclaimer">Press <i>&quot;Connect Wallet&quot;</i> to sign-up with email or crypto wallet and claim your digital collectable</p>
           </div>
 
@@ -179,7 +165,6 @@ export default async function ClaimPage({
             ))}
           </div>
         </div>
-        {/* <NFTAccordion /> */}
       </div>
       <SimpleFooter />
     </>
