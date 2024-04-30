2fd5604771ac2fd8b679111d13411a145fe592c6
Christopher Norman <normtronics@gmail.com>
2024-04-29 19:54:18 -0700

    Adding analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { OnBoarding } from "@/components/onboarding/onboarding";
 import type { Metadata } from "next";
 import "./globals.css";
 import { Providers } from './providers'
+import { Analytics } from "@vercel/analytics/react"
 
 const title = 'Ruby Mountain NFTs'
 const description = 'Ruby Mountain NFTs'
@@ -57,6 +58,7 @@ export default function RootLayout({
           {children}
           {/* <OnBoarding /> */}
           <AudioPlayer />
+          <Analytics/>
         </Providers>
       </body>
     </html>
