b8030356e4615ea3a1c09328ffab713151f01506
Christopher Norman <chris@artofdigging.com>
2024-04-30 14:55:28 -0700

    Adding stripe webhook

---
+++ src/app/api/webhooks/stripe/checkout/route.ts
@@ -0,0 +1,74 @@
+import Stripe from "stripe";
+import { NextRequest } from "next/server";
+import { headers } from "next/headers";
+import { Engine } from "@thirdweb-dev/engine";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2024-04-10",
+  typescript: true,
+});
+
+export async function POST(request: NextRequest) {
+  const body = await request.text();
+  const endpointSecret = process.env.STRIPE_SECRET_WEBHOOK_KEY!;
+  const BACKEND_WALLET_ADDRESS = process.env.BACKEND_WALLET_ADDRESS!;
+  const CHAIN = process.env.NEXT_PUBLIC_CHAIN!;
+
+  const sig = headers().get("stripe-signature") as string;
+  let event: Stripe.Event;
+  try {
+    event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
+  } catch (err) {
+    return new Response(`Webhook Error: ${err}`, {
+      status: 400,
+    });
+  }
+
+  switch (event.type) {
+    case "checkout.session.async_payment_failed":
+      const checkoutSessionAsyncPaymentFailed = event.data.object;
+      break;
+    case "checkout.session.async_payment_succeeded":
+      const checkoutSessionAsyncPaymentSucceeded = event.data.object;
+
+      const engine = new Engine({
+        url: process.env.ENGINE_URL!,
+        accessToken: process.env.THIRDWEB_ACCESS_TOKEN!,
+      });
+
+      const { nft } = await getData(
+        checkoutSessionAsyncPaymentSucceeded.metadata?.slug
+      );
+
+      const tx = await engine.erc721.mintTo(
+        CHAIN,
+        checkoutSessionAsyncPaymentSucceeded.metadata?.contractAddress || "",
+        BACKEND_WALLET_ADDRESS,
+        {
+          metadata: {
+            name: nft.title,
+            description: nft.description,
+            image: nft.image,
+            //@ts-ignore
+            properties: nft.atributes,
+          },
+          receiver:
+            checkoutSessionAsyncPaymentSucceeded.metadata?.address || "",
+        }
+      );
+
+      break;
+    case "checkout.session.completed":
+      const checkoutSessionCompleted: any = event.data.object;
+
+      break;
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+  return new Response("RESPONSE EXECUTE", {
+    status: 200,
+  });
+}
+function getData(slug: any): { nft: any } | PromiseLike<{ nft: any }> {
+  throw new Error("Function not implemented.");
+}
