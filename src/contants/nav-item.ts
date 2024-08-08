1a8f5f12d46e5d6faeaa1da9b94f09fa22875577
Christopher Norman <chris@artofdigging.com>
2024-08-08 09:32:32 -0700

    Updates

---
+++ src/contants/nav-item.ts
@@ -0,0 +1,82 @@
+export const items = [
+  {
+    top: "Booking",
+    options: [
+      {
+        link: "https://www.rubymountain.xyz/contact",
+        name: "Contact Me",
+      },
+    ],
+  },
+  {
+    top: "Follow",
+    options: [
+      {
+        link: "https://www.youtube.com/@rubymountain",
+        name: "Youtube",
+      },
+      {
+        link: "https://www.instagram.com/ruby.mountain/",
+        name: "Instagram",
+      },
+      {
+        link: "https://twitter.com/IAmRubyMountain",
+        name: "Twitter",
+      },
+      {
+        link: "https://www.tiktok.com/@rubymountain",
+        name: "TikTok",
+      },
+      {
+        link: "https://www.pinterest.com/iamrubymountain/",
+        name: "Pintrest",
+      },
+    ],
+  },
+  {
+    top: "Listen",
+    options: [
+      {
+        link: "https://music.youtube.com/channel/UCZrkMbaDfQvFkcwCNf8fikw",
+        name: "Youtube Music",
+      },
+      {
+        link: "https://music.apple.com/us/artist/1434165588",
+        name: "Apple Music",
+      },
+      {
+        link: "https://rubymountain.bandcamp.com/",
+        name: "Bandcamp",
+      },
+      {
+        link: "https://open.spotify.com/artist/6z8x3SY9zhlLv8RVdmLNaG",
+        name: "Spotify",
+      },
+    ],
+  },
+];
+
+// const footer = [{
+//   name: 'Terms of use',
+//   link: '/terms'
+// },
+// {
+//   name: 'Privacy policy',
+//   link: '/terms'
+// },{
+//   name: 'Do not share my personal information',
+//   link: '/terms'
+// },{
+//   name: 'California policy',
+//   link: '/terms'
+// },{
+//   name: 'Site map',
+//   link: '/terms'
+// },{
+//   name: 'Public Notice',
+//   link: '/terms'
+// },
+// {
+//   name: 'Accessibility Statement',
+//   link: '/terms'
+// }]
